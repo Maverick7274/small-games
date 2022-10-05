3fb8bc2c0f44e9ac9855d5b833e34c47b49eac14
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2022-10-05 18:15:31 +0530

    Added ttf file for retro-font

--- pong/pong.py
+++ pong/pong.py
@@ -11,6 +11,11 @@ wn.setup(width=1280, height=720)
 wn.tracer(0)
 
 
+# Score keeping
+score_a = 0
+score_b = 0
+
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -38,6 +43,19 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
+
+
+# Pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("BigBlue Terminal 437TT Nerd Font Complete", 24, "normal"))
 
 
 
@@ -46,8 +64,71 @@ ball.goto(0, 0)
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard Bindings
+
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
 
 
 
+wn.onkeypress(paddle_b_up, "i")
+wn.onkeypress(paddle_b_down, "k")
+
+
+# Main Game Loop
 while True:
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border Checking
+    if ball.ycor() > 350:
+        ball.sety(350)
+        ball.dy *= -1
+
+    if ball.ycor() < -350:
+        ball.sety(-350)
+        ball.dy *= -1
+
+    if ball.xcor() > 620:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("BigBlue Terminal 437TT Nerd Font Complete", 24, "normal"))
+
+    if ball.xcor() < -620:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("BigBlue Terminal 437TT Nerd Font Complete", 24, "normal"))
+
+    # Paddle and Ball Collisions
+
+    if (ball.xcor() > 540 and ball.xcor() < 550) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(540)
+        ball.dx *= -1
+    if (ball.xcor() < -540 and ball.xcor() > -550) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-540)
+        ball.dx *= -1
